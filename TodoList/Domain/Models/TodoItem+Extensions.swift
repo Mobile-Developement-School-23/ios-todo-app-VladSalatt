6a76d650812882e7906f7fbace34ad25a73d1ab7
VKoshelev <VKoshelev@detmir.ru>
2023-06-14 00:30:01 +0300

    add CustomKeys to custom failable init

--- TodoList/Domain/Models/TodoItem+Extensions.swift
+++ TodoList/Domain/Models/TodoItem+Extensions.swift
@@ -4,6 +4,7 @@ extension TodoItem {
     static func parse(json: Any) -> TodoItem? {
         var data: Data?
 
+        // Такой маппинг нужен для того, чтобы обрабатывать различные входные типы данных
         switch json {
         case is Data:
             data = json as? Data
@@ -35,23 +36,23 @@ private extension TodoItem {
     /// Инициализатор, который преобразует словарь типа [String: Any] (aka json) в объект типа TodoItem
     init?(from jsonDict: [String: Any]) {
         guard
-            let id = jsonDict["id"] as? String,
-            let text = jsonDict["text"] as? String,
-            let importanceRaw = jsonDict["importance"] as? String,
+            let id = jsonDict[CustomKeys.id.rawValue] as? String,
+            let text = jsonDict[CustomKeys.text.rawValue] as? String,
+            let importanceRaw = jsonDict[CustomKeys.importance.rawValue] as? String,
             let importance = Importance(rawValue: importanceRaw),
-            let isDone = jsonDict["done"] as? Bool,
-            let createdAtTS = jsonDict["created_at"] as? TimeInterval
+            let isDone = jsonDict[CustomKeys.isDone.rawValue] as? Bool,
+            let createdAtTS = jsonDict[CustomKeys.createdAt.rawValue] as? TimeInterval
         else { return nil }
         
         let createdAt = Date(timeIntervalSince1970: createdAtTS)
         
         let deadline: Date? = {
-            guard let deadline = jsonDict["deadline"] as? TimeInterval else { return nil }
+            guard let deadline = jsonDict[CustomKeys.deadline.rawValue] as? TimeInterval else { return nil }
             return Date(timeIntervalSince1970: deadline)
         }()
         
         let changedAt: Date? = {
-            guard let changedAt = jsonDict["changed_at"] as? TimeInterval else { return nil }
+            guard let changedAt = jsonDict[CustomKeys.changedAt.rawValue] as? TimeInterval else { return nil }
             return Date(timeIntervalSince1970: changedAt)
         }()
         
